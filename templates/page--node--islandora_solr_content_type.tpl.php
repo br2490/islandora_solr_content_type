79da46107f5405f3fcd9ac9ac9a3f6652932624a
Benjamin Rosner <brosner@barnard.edu>
2016-04-06 19:35:58 +0000

    Code cleanup, docgen, etc...

--- templates/page--node--islandora_solr_content_type.tpl.php
+++ templates/page--node--islandora_solr_content_type.tpl.php
@@ -2,7 +2,6 @@
 /**
  * @file
  * Returns the HTML for a single Drupal page.
- *
  * Complete documentation for this file is available online.
  * @see https://drupal.org/node/1728148
  */
@@ -13,23 +12,26 @@
   <header class="header" id="header" role="banner">
 
     <?php if ($logo): ?>
-      <a href="http://barnard.edu" title="Barnard College" class="header__logo" id="logo"><img
-          src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image"/></a>
+      <a href="http://barnard.edu" title="Barnard College" class="header__logo"
+         id="logo"><img
+          src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"
+          class="header__logo-image"/></a>
     <?php endif; ?>
 
     <?php if ($secondary_menu && !empty($secondary_menu_heading)): ?>
       <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
-        <?php print theme('links__system_secondary_menu', array(
-          'links' => $secondary_menu,
-          'attributes' => array(
-            'class' => array('links', 'inline', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => $secondary_menu_heading,
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
+        <?php print theme('links__system_secondary_menu',
+                          array(
+                            'links'      => $secondary_menu,
+                            'attributes' => array(
+                              'class' => array('links', 'inline', 'clearfix'),
+                            ),
+                            'heading'    => array(
+                              'text'  => $secondary_menu_heading,
+                              'level' => 'h2',
+                              'class' => array('element-invisible'),
+                            ),
+                          )); ?>
       </nav>
     <?php endif; ?>
 
@@ -42,13 +44,16 @@
       <div class="header__name-and-slogan" id="name-and-slogan">
         <?php if ($site_name): ?>
           <h1 class="header__site-name" id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"
-               class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
+            <a href="<?php print $front_page; ?>"
+               title="<?php print t('Home'); ?>"
+               class="header__site-link"
+               rel="home"><span><?php print $site_name; ?></span></a>
           </h1>
         <?php endif; ?>
 
         <?php if ($site_slogan): ?>
-          <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
+          <div class="header__site-slogan"
+               id="site-slogan"><?php print $site_slogan; ?></div>
         <?php endif; ?>
       </div>
     <?php endif; ?>
@@ -60,17 +65,18 @@
         // "Main menu" on your sub-theme's settings form, deleting this PHP
         // code block, and, instead, using the "Menu block" module.
         // @see https://drupal.org/project/menu_block
-        print theme('links__system_main_menu', array(
-          'links' => $main_menu,
-          'attributes' => array(
-            'class' => array('links', 'inline', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => t('Main menu'),
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
+        print theme('links__system_main_menu',
+                    array(
+                      'links'      => $main_menu,
+                      'attributes' => array(
+                        'class' => array('links', 'inline', 'clearfix'),
+                      ),
+                      'heading'    => array(
+                        'text'  => t('Main menu'),
+                        'level' => 'h2',
+                        'class' => array('element-invisible'),
+                      ),
+                    )); ?>
       </nav>
     <?php endif; ?>
 
@@ -86,12 +92,14 @@
         <?php print $bc_breadcrumb; ?>
       <?php endif; ?>
       <?php
-      $bc_views_breadcrumb = array('breadcrumb' => menu_get_active_breadcrumb());
+      $bc_views_breadcrumb =
+        array('breadcrumb' => menu_get_active_breadcrumb());
       print theme('bc_islandora_breadcrumb', $bc_views_breadcrumb);
       isset($breadcrumb) ? print $breadcrumb : NULL;
       ?>
       <?php if ($title): ?>
-        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+        <h1 class="page__title title"
+            id="page-title"><?php print $title; ?></h1>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>
@@ -101,7 +109,8 @@
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
       <div class="results"><span
-          class="results-for"><?php print t('Results for'); ?></span><?php print $breadcrumb; ?></div>
+          class="results-for"><?php print t('Results for'); ?></span><?php print $breadcrumb; ?>
+      </div>
       <?php print render($page['content']); ?>
       <?php print $feed_icons; ?>
       <?php if (isset($socialmedia)): ?>
