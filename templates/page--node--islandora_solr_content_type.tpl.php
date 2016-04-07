90e6c253c6d0f37b6206bdb90098c32513894f0e
Benjamin Rosner <brosner@barnard.edu>
2016-04-07 01:38:20 +0000

    Code cleanup...

--- templates/page--node--islandora_solr_content_type.tpl.php
+++ templates/page--node--islandora_solr_content_type.tpl.php
@@ -20,18 +20,21 @@
 
     <?php if ($secondary_menu && !empty($secondary_menu_heading)): ?>
       <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
-        <?php print theme('links__system_secondary_menu',
-                          array(
-                            'links'      => $secondary_menu,
-                            'attributes' => array(
-                              'class' => array('links', 'inline', 'clearfix'),
-                            ),
-                            'heading'    => array(
-                              'text'  => $secondary_menu_heading,
-                              'level' => 'h2',
-                              'class' => array('element-invisible'),
-                            ),
-                          )); ?>
+        <?php print theme('links__system_secondary_menu', array(
+            'links'      => $secondary_menu,
+            'attributes' => array(
+              'class' => array(
+                'links',
+                'inline',
+                'clearfix',
+              ),
+            ),
+            'heading'    => array(
+              'text'  => $secondary_menu_heading,
+              'level' => 'h2',
+              'class' => array('element-invisible'),
+            ),
+          )); ?>
       </nav>
     <?php endif; ?>
 
@@ -65,18 +68,21 @@
         // "Main menu" on your sub-theme's settings form, deleting this PHP
         // code block, and, instead, using the "Menu block" module.
         // @see https://drupal.org/project/menu_block
-        print theme('links__system_main_menu',
-                    array(
-                      'links'      => $main_menu,
-                      'attributes' => array(
-                        'class' => array('links', 'inline', 'clearfix'),
-                      ),
-                      'heading'    => array(
-                        'text'  => t('Main menu'),
-                        'level' => 'h2',
-                        'class' => array('element-invisible'),
-                      ),
-                    )); ?>
+        print theme('links__system_main_menu', array(
+          'links'      => $main_menu,
+          'attributes' => array(
+            'class' => array(
+              'links',
+              'inline',
+              'clearfix',
+            ),
+          ),
+          'heading'    => array(
+            'text'  => t('Main menu'),
+            'level' => 'h2',
+            'class' => array('element-invisible'),
+          ),
+        )); ?>
       </nav>
     <?php endif; ?>
 
